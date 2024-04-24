5d98352951af8efb95600d01477c4f2909635dd2
Snipez-2412 <promegasniper2412@gmail.com>
2024-04-24 14:59:54 +0700

    Update, Add for Category

---
+++ AddCategory.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddCategory.aspx.cs" Inherits="MobileStore.AddCategory" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Add New Product</title>
+</head>
+<body>
+    <form id="addCategory" runat="server">
+        <div>
+            <label for="txtCategoryID">Category ID:</label>
+            <asp:TextBox ID="txtCategoryID" runat="server"></asp:TextBox><br />
+
+            <label for="txtCategoryName">Category Name:</label>
+            <asp:TextBox ID="txtCategoryName" runat="server"></asp:TextBox><br 
+
+            <asp:Button ID="btnAddCategory" runat="server" Text="Add Category" OnClick="btnAddCategory_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ UpdateCategory.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MobileStore
+{
+    public partial class UpdateCategory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Load data into form for editing
+                if (Request.QueryString["CategoryID"] != null)
+                {
+                    string categoryId = Request.QueryString["CategoryID"];
+                    LoadCategoryData(categoryId);
+                }
+            }
+        }
+
+        //Load Data
+        protected void LoadCategoryData(string categoryId)
+        {
+            // Fetch data from database and populate form
+            SqlConnection myCon = new SqlConnection(ConfigurationManager.ConnectionStrings["MobileStore"].ConnectionString);
+            using (myCon)
+            {
+                string query = "SELECT CategoryName FROM Category WHERE CategoryID = @CategoryID";
+                SqlCommand command = new SqlCommand(query, myCon);
+                command.Parameters.AddWithValue("@CategoryID", categoryId);
+
+                myCon.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    txtCategoryName.Text = reader["CategoryName"].ToString();
+                }
+                reader.Close();
+            }
+        }
+
+        // Update Button
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            // Update data in database
+            SqlConnection myCon = new SqlConnection(ConfigurationManager.ConnectionStrings["MobileStore"].ConnectionString);
+            using (myCon)
+            {
+                string query = "UPDATE Category SET CategoryName = @CategoryName WHERE CategoryID = @CategoryID";
+                SqlCommand command = new SqlCommand(query, myCon);
+                command.Parameters.AddWithValue("@CategoryID", Request.QueryString["CategoryID"]);
+                command.Parameters.AddWithValue("@CategoryName", txtCategoryName.Text);
+
+                myCon.Open();
+                command.ExecuteNonQuery();
+            }
+
+            // Redirect to a confirmation page or back to Category list
+            Response.Redirect("Admin.aspx");
+        }
+    }
+}
